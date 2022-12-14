718ed704df3edeaaa1388d24c0a743c40218266c
vanillovin <vanillovin@gmail.com>
2022-12-14 21:55:24 +0900

    fix: user state가 변경되면 로컬스토리지 USER 키에 값 설정

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext(null);
 
@@ -6,6 +6,10 @@ function UserProvider({ children }) {
   const [user, setUser] = useState(localStorage.getItem('USER'));
 
   const onSetUser = (value) => setUser(value);
+
+  useEffect(() => {
+    localStorage.setItem('USER', user);
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, onSetUser }}>{children}</UserContext.Provider>
