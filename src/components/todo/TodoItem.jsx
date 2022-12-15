e9f166255f96ae266b4168793fa9852c1cd93a90
vanillovin <vanillovin@gmail.com>
2022-12-15 10:20:44 +0900

    feat: 투두 업데이트, 삭제

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toDoAPI } from '../../apis';
 
 function TodoItem({ token, todo, setTodos }) {
+  const [text, setText] = useState(todo.todo);
+  const [editing, setEditing] = useState(false);
+
   const handleDeleteTodo = () => {
     toDoAPI
       .deleteTodo(token, todo.id)
@@ -16,24 +19,105 @@ function TodoItem({ token, todo, setTodos }) {
       });
   };
 
+  const handleCompleteTodo = () => {
+    toDoAPI
+      .updateTodo(token, {
+        id: todo.id,
+        data: { todo: todo.todo, isCompleted: !todo.isCompleted },
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) throw new Error(data.message);
+        setTodos((prev) => ({
+          ...prev,
+          data: prev.data.map((prevTodo) =>
+            prevTodo.id === todo.id
+              ? { ...prevTodo, isCompleted: data.isCompleted }
+              : prevTodo
+          ),
+        }));
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  const handleUpdateTodo = (e) => {
+    e.preventDefault();
+    toDoAPI
+      .updateTodo(token, {
+        id: todo.id,
+        data: { todo: text, isCompleted: todo.isCompleted },
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) throw new Error(data.message);
+        setEditing(false);
+        setTodos((prev) => ({
+          ...prev,
+          data: prev.data.map((prevTodo) =>
+            prevTodo.id === todo.id ? { ...prevTodo, todo: data.todo } : prevTodo
+          ),
+        }));
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
-    <li className="flex items-center justify-between my-2">
+    <li className="my-2">
       {/* isCompleted, userId */}
-      <p className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.todo}</p>
-      <div>
-        <button
-          onClick={handleDeleteTodo}
-          className="border rounded-full w-7 h-7 text-rose-400 mr-1 hover:bg-gray-100 transition-all"
-        >
-          ✘
-        </button>
-        <button
-          onClick={() => {}}
-          className="border rounded-full w-7 h-7 text-emerald-400 hover:bg-gray-100 transition-all"
-        >
-          ✔
-        </button>
-      </div>
+      {!editing ? (
+        <div className="flex items-center justify-between">
+          <p className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.todo}</p>
+          <div>
+            <button
+              onClick={() => setEditing(true)}
+              className="border rounded-full w-7 h-7 text-amber-400 mr-1 hover:bg-gray-100 transition-all"
+            >
+              ✏
+            </button>
+            <button
+              onClick={handleDeleteTodo}
+              className="border rounded-full w-7 h-7 text-rose-400 mr-1 hover:bg-gray-100 transition-all"
+            >
+              ✘
+            </button>
+            <button
+              onClick={handleCompleteTodo}
+              className="border rounded-full w-7 h-7 text-emerald-400 hover:bg-gray-100 transition-all"
+            >
+              ✔
+            </button>
+          </div>
+        </div>
+      ) : (
+        <form className="flex items-center justify-between" onSubmit={handleUpdateTodo}>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="border flex-1 p-1 outline-none"
+          />
+          <div>
+            <button
+              className={`rounded-sm ml-1 ${
+                todo.todo === text ? 'bg-red-100' : 'bg-red-300'
+              } p-1 hover:bg-opacity-70`}
+              disabled={todo.todo === text}
+              onClick={handleUpdateTodo}
+            >
+              수정
+            </button>
+            <button
+              className="rounded-sm ml-1 bg-red-200 p-1 hover:bg-opacity-70"
+              onClick={() => setEditing(false)}
+            >
+              취소
+            </button>
+          </div>
+        </form>
+      )}
     </li>
   );
 }
